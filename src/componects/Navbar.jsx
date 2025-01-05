b2a531bac826d4181eeb6b7234504ded6f2fbf58
Sasika Dilum <sasikadilum40@gmail.com>
2025-01-05 11:22:56 +0530

    s

--- src/componects/Navbar.jsx
+++ src/componects/Navbar.jsx
@@ -1,5 +1,5 @@
 import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
-import logo from '../assets/sasika1.png';
+import logo from '../assets/sasika.jpg';
 
 const Navbar = () => {
     return (
