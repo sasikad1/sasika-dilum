707d4c8fe6b65926d0acce1be9be750443931761
Sasika Dilum <sasikadilum40@gmail.com>
2025-01-05 10:39:02 +0530

    logo change

--- src/componects/Navbar.jsx
+++ src/componects/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <nav className="flex items-center justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
                 <a href="/" aria-label="Home">
-                    <img src={logo} className="mx-2" width={100} height={33} alt="Logo"/> 
+                    <img src={logo} className="mx-2" width={120} height={33} alt="Logo"/> 
                 </a>
             </div>
             <div className="m-8 flex items-center justify-center gap-4 text-2xl">
