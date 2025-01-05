39922c47e2ade08863e5773019c4df4b8ed01727
Sasika Dilum <sasikadilum40@gmail.com>
2025-01-05 08:56:55 +0530

    chnage log

--- src/componects/Navbar.jsx
+++ src/componects/Navbar.jsx
@@ -1,5 +1,5 @@
 import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
-import logo from "../assets/raviKumarLogo.webp/";
+import logo from "../assets/lo.jpg/";
 
 const Navbar = () => {
     return (
